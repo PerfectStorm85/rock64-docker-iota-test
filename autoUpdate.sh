827fa46d8fe67b738e6d88ab4dcdf7a44910a23b
PerfectStorm85 <info@ronkamphuis.nl>
2018-07-26 20:02:51 +0000

    Added auto update property

--- autoUpdate.sh
+++ autoUpdate.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
+# Testing only!
+testVersion="False"
+
+if [ "$testVersion" == "True" ]
+then
+        IRIVersion="IRIVersionTest.html"
+	updateScript="updateScriptTest.sh"
+else
+        IRIVersion="IRIVersion.html"
+        updateScript="updateScript.sh"
+fi
+
 while [ true ]
 do
         # Check if latest version is already running. If not, download updateScript.sh and run it.
         # This can contain updates to the github, like the docker-container.
-
-        content=$(wget https://www.Rock64Iota.com/Iota/IRIVersion.html -q -O -)
+	content=$(wget https://www.Rock64Iota.com/Iota/$IRIVersion -q -O -)
         # If empty our internet connection might be lost. In that case don't do anything.
         if [ "$content" != "" ]
         then
@@ -14,13 +25,14 @@ do
         if [ "$curVersion" != "$content" ]
         then
                 echo "$content" > CurVersion.ini
-                wget https://www.Rock64Iota.com/Iota/updateScript.sh
-                chmod +x ./updateScript.sh
+		rm ./updateScript*
+                wget https://www.Rock64Iota.com/Iota/$updateScript
+                chmod +x ./$updateScript
 
                 #The update script is usually empty. But in case something else needs to be updated on the rock itself this script is called and executed.
                 #I will never use this method to push harmful content to your device!
 
-                ./updateScript.sh
+                ./$updateScript
                 echo "Updating... Disabeling IRI, Nelson and Field..."
                 docker-compose stop
                 docker-compose down
@@ -31,8 +43,13 @@ do
                 docker rmi $(docker images -q)
                 echo "Restarting IRI..."
                 docker-compose up -d
-                rm ./updateScript.sh
+                rm ./$updateScript
         fi
         fi
-        sleep 600s
+	if [ "$testVersion" == "True" ]
+	then
+		sleep 6s
+	else
+        	sleep 600s
+	fi
 done
